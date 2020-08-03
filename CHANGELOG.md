df4c1a6f36ace72052ed152403ab787ba956e83c
Nichlas Severinsen <ns@nsz.no>
2020-08-03 11:33:00 +0200

    Release 0.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [0.0.4] - 2020-08-03
+### Fixed
+- Build
+
 ## [0.0.3] - 2020-08-03
 ### Changed
 - Default output iso name is game_id.iso instead of output.iso
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
   name="libray",
-  version="0.0.3",
+  version="0.0.4",
   description='A Libre (FLOSS) Python application for unencrypting, extracting, repackaging, and encrypting PS3 ISOs',
   author="Nichlas Severinsen",
   author_email="ns@nsz.no",
@@ -14,7 +14,7 @@ setup(
   scripts=['libray/libray'],
   install_requires=[
     'tqdm==4.32.2',
-    'pycrypto==2.6.1',
+    'pycryptodome==3.9.8',
     'requests==2.22.0',
     'beautifulsoup4==4.7.1',
   ],
